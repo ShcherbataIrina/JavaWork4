bfccd92a1a729db6f461e47e713f5c38495f8289
ShcherbataIrina <serbataairina@gmail.com>
2023-02-05 17:24:57 +0200

    HomeWork4

--- src/main/java/ua/ithillel/lesson4/ArrayValueCalculator.java
+++ src/main/java/ua/ithillel/lesson4/ArrayValueCalculator.java
@@ -1,23 +1,24 @@
 package ua.ithillel.lesson4;
 
 public class ArrayValueCalculator {
-    public static final int SIZE_ARRAY = 4;
+    public static final int ARRAY_SIZE = 4;
 
-    public int doCalc(String[][] myArray) throws ArraySizeException, ArrayDataException {
+    public int doCalc(String[][] myArray){
 
-        if (myArray.length != SIZE_ARRAY) {
+        if (myArray.length != ARRAY_SIZE) {
             throw new ArraySizeException("Array must have 4x4 !");
         }
 
         int sum = 0;
-        for (int i = 0; i < SIZE_ARRAY; i++) {
-            for (int j = 0; j < SIZE_ARRAY; j++) {
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            for (int j = 0; j < ARRAY_SIZE; j++) {
                 try {
                     sum += Integer.parseInt(myArray[i][j]);
 
                 } catch (NumberFormatException ex) {
                     ex.printStackTrace();
                     throw new ArrayDataException(ex);
+
                 }
 
             }
--- src/test/java/ua/ithillel/lesson4/ArrayValueCalculatorTest.java
+++ src/test/java/ua/ithillel/lesson4/ArrayValueCalculatorTest.java
@@ -2,50 +2,85 @@ package ua.ithillel.lesson4;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayValueCalculatorTest {
     @Test
-    void test1() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
-        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
-        assertEquals(110, array.doCalc(myArray));
-    }
-
-    @Test
-    void test2() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
-        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"0", "2", "1", "10"}, {"17", "8", "11", "12"}};
-        assertEquals(104, array.doCalc(myArray));
-    }
-
-    @Test
-    void test3() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
-        String[][] myArray = {{"5", "3", "1", "9"}, {"5", "-28", "1", "7"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
-        assertEquals(70, array.doCalc(myArray));
-    }
-
-    @Test
-    void test4() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
-        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"6", "2", "1", "10"}, {"-17", "8", "11", "12"}};
-        assertEquals(76, array.doCalc(myArray));
-    }
-
-    @Test
-    void test5() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
+    void shouldThrowExceptionOnParsingIncorrectString1() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
         String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"6", "d", "1", "10"}, {"17", "8", "11", "12"}};
-        assertEquals(110, array.doCalc(myArray));
+        assertThrows(ArrayDataException.class, new Executable() {
+            @Override
+            public void execute() throws RuntimeException {
+                calculator.doCalc(myArray);
+            }
+        });
     }
 
     @Test
-    void test6() throws ArrayDataException {
-        ArrayValueCalculator array = new ArrayValueCalculator();
-        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "z"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
-        assertEquals(110, array.doCalc(myArray));
+    void shouldThrowExceptionOnParsingIncorrectString2() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "RIVER"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
+        assertThrows(ArrayDataException.class, new Executable() {
+            @Override
+            public void execute() throws RuntimeException {
+                calculator.doCalc(myArray);
+            }
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionOnWrongSizeString1() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1"}, {"5", "8", "1", "2"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
+        assertThrows(ArraySizeException.class, new Executable() {
+            @Override
+            public void execute() throws RuntimeException {
+                calculator.doCalc(myArray);
+            }
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionOnWrongSizeString2() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "2"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}, {"1", "1", "1", "1"}};
+        assertThrows(ArraySizeException.class, new Executable() {
+            @Override
+            public void execute() throws RuntimeException {
+                calculator.doCalc(myArray);
+            }
+        });
+    }
+
+    @Test
+    void test1() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
+        assertEquals(110, calculator.doCalc(myArray));
+    }
+
+    @Test
+    void test2() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"0", "2", "1", "10"}, {"17", "8", "11", "12"}};
+        assertEquals(104, calculator.doCalc(myArray));
+    }
+
+    @Test
+    void test3() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "3", "1", "9"}, {"5", "-28", "1", "7"}, {"6", "2", "1", "10"}, {"17", "8", "11", "12"}};
+        assertEquals(70, calculator.doCalc(myArray));
+    }
+
+    @Test
+    void test4() {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+        String[][] myArray = {{"5", "7", "1", "9"}, {"5", "8", "1", "7"}, {"6", "2", "1", "10"}, {"-17", "8", "11", "12"}};
+        assertEquals(76, calculator.doCalc(myArray));
     }
 
 }
